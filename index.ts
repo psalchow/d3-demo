5c9ab1dd0b69d9636d04031df50c5459a0e5781b
Peter Salchow <p.salchow.extern@eos-ts.com>
2020-11-30 14:26:45 +0100

    Add caption

--- index.ts
+++ index.ts
@@ -9,17 +9,24 @@ root.style("height", DIAGRAM_HEIGHT + 'px');
 
 const draw = (data: LanguageData[]) => {
 	const scale = DIAGRAM_HEIGHT / Math.max(...data.map((e) => e.value));
-	root
+	const chartEntry = root
 		.selectAll('div.divchart')
 		.data(data)
 		.enter()
 		.append("div")
+			.attr("class", "chart-entry");
+
+		chartEntry.append("div")
 			.style("height", "0px")
-			.attr("class", "divchart")
+			.attr("class", "chart")
 			.transition()
 			.duration(1000)
 			.style("height", (d) => (d.value * scale) + "px")
 			.text((d, i, a) => d.value);
+
+		chartEntry.append("div")
+			.attr("class", "chart-caption")
+			.text((d) => d.language);
 }
 
 draw(data);
