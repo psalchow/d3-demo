4cfe7e797f493242dee3c1c12a2477e4c4daa7f5
psalchow <peter.salchow@iteratec.com>
2020-11-30 10:23:25 +0100

    4 files modified

--- index.ts
+++ index.ts
@@ -1,6 +1,18 @@
-// Import stylesheets
+import * as d3 from 'd3';
 import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+var data = [40, 80, 150, 160, 230, 260];
+var body = d3.select("body");
+
+draw(data);
+
+function draw(dataset){
+	var divs = body.selectAll('div').data(data);
+	
+	divs.enter().append("div").style("width", "0px");
+	
+	divs.transition().duration(1000).style("width", function(d) { return d + "px"; })
+ 	.attr("class", "divchart")
+    .text(function(d) { return d; });
+		
+}
