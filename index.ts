fa9376d78f8ba113e5c9904529b514ef4bcf3142
Peter Salchow <p.salchow.extern@eos-ts.com>
2020-11-30 10:33:00 +0100

    add types and code cleanup

--- index.ts
+++ index.ts
@@ -1,18 +1,17 @@
 import * as d3 from 'd3';
 import './style.css';
 
-var data = [40, 80, 150, 160, 230, 260];
-var body = d3.select("body");
+const data = [40, 80, 150, 160, 230, 260];
+const body = d3.select("body");
 
-draw(data);
+const draw = (dataset: number[]) => {
+	const divs = body.selectAll('div').data(dataset);
 
-function draw(dataset){
-	var divs = body.selectAll('div').data(data);
-	
 	divs.enter().append("div").style("width", "0px");
-	
+
 	divs.transition().duration(1000).style("width", function(d) { return d + "px"; })
  	.attr("class", "divchart")
-    .text(function(d) { return d; });
-		
+    .text((d) => d);
 }
+
+draw(data);
