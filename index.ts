c9b584dec168414c225d48ff9cc008399274b735
Peter Salchow <p.salchow.extern@eos-ts.com>
2020-12-01 06:57:45 +0100

    Add hover in effect

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
 import './style.css';
-import {LanguageData, data} from "./data";
+import {data, LanguageData} from "./data";
 
 const DIAGRAM_HEIGHT = 400; // in px
 
@@ -8,25 +8,28 @@ const root = d3.select("div.chart-container");
 root.style("height", DIAGRAM_HEIGHT + 'px');
 
 const draw = (data: LanguageData[]) => {
-	const scale = (DIAGRAM_HEIGHT - 64) / Math.max(...data.map((e) => e.value));
-	const chartEntry = root
-		.selectAll('div.divchart')
-		.data(data)
-		.enter()
-		.append("div")
-			.attr("class", "chart-entry");
+    const scale = (DIAGRAM_HEIGHT - 64) / Math.max(...data.map((e) => e.value));
+    const chartEntry = root
+        .selectAll('div.divchart')
+        .data(data)
+        .enter()
+        .append("div")
+        .attr("class", "chart-entry");
 
-		chartEntry.append("div")
-			.style("height", "0px")
-			.attr("class", "chart")
-			.transition()
-			.duration(1000)
-			.style("height", (d) => (d.value * scale) + "px")
-			.text((d, i, a) => d.value);
+    chartEntry.append("div")
+        .style("height", "0px")
+        .attr("class", "chart")
+		.on('mouseenter', (event, data) => {
+			d3.select(event.currentTarget).transition().duration(100).style("margin-left", 0).style("margin-right", 0)
+		})
+        .transition()
+        .duration(1000)
+        .style("height", (d) => (d.value * scale) + "px")
+        .text((d) => d.value);
 
-		chartEntry.append("div")
-			.attr("class", "chart-caption")
-			.text((d) => d.language);
-}
+    chartEntry.append("div")
+        .attr("class", "chart-caption")
+        .text((d) => d.language);
+};
 
 draw(data);
